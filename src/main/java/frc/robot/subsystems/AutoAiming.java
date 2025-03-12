71909db0fe98096bf9d51f9b674c54fe5ac874b8
Shirley <125406552+0chens@users.noreply.github.com>
2025-03-11 22:20:53 -0400

    Final Autoaiming & settup (should be ready for competition)

--- src/main/java/frc/robot/subsystems/AutoAiming.java
+++ src/main/java/frc/robot/subsystems/AutoAiming.java
@@ -18,6 +18,7 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructPublisher;
 import frc.robot.Constants;
+import frc.robot.RobotContainer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance; //may not need both?
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -34,7 +35,7 @@ public class AutoAiming extends SubsystemBase {
   }
 
 
-//Method to return the position of the branch closest to it.
+//Method to return the position of the branchCon closest to it.
   public Translation2d chooseBranch (Translation2d robotPose){
     double closest = 10000000000000.0;
     int ind = -1;
@@ -57,11 +58,11 @@ public class AutoAiming extends SubsystemBase {
     Rotation2d robotRotation = coralHandlerSupplier.get().getRotation().toRotation2d();
 
 //IN DEGREES NOT RADIANS!!!!
-    Rotation2d initialAngle = new Rotation2d((robotRotation.getDegrees() - changeNeeded.getAngle().getDegrees()));
+    Rotation2d initialAngle = new Rotation2d(-(robotRotation.getDegrees() - changeNeeded.getAngle().getDegrees()));
     Rotation2d finalAngle;
     
 
-    /*
+    
     if ((initialAngle.getDegrees() < 80) && (initialAngle.getDegrees() > (-80))){
       finalAngle = changeNeeded.getAngle();
     }else if(initialAngle.getDegrees() > 280){
@@ -69,9 +70,9 @@ public class AutoAiming extends SubsystemBase {
     }else if(initialAngle.getDegrees() < -280){
       finalAngle = new Rotation2d(initialAngle.getDegrees()+360);
     }else{
-      finalAngle = new Rotation2d();
+      finalAngle = RobotContainer.coralHandler.getHorizontalAngle();
     }
-    */
+  
     finalAngle = initialAngle;
 
     return finalAngle;
@@ -84,7 +85,7 @@ public Rotation2d verticalRotationToCoral (){
   double horizontalChangeNeeded = coralHandlerSupplier.get().getTranslation().toTranslation2d().getDistance(chooseBranch(coralHandlerSupplier.get().toPose2d().getTranslation()));
   
   double closest = 100000.0;
-  for(int i = 0; i<Constants.CoralPlacement.heightsCoral.size(); i++){
+  for(int i = 0; i<stants.CoralPlacement.heightsCoral.size(); i++){
     if(Constants.CoralPlacement.heightsCoral.get(i) < closest){
       closest = Constants.CoralPlacement.heightsCoral.get(i);
     }
